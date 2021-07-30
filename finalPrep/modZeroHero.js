0d08ef5148d92910fef80cee3d526fc765befa9c
Emily-Cathleen <cathleencorcoran@gmail.com>
2021-07-29 23:18:31 -0400

    Update modZeroHero.js

--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -48,13 +48,13 @@ console.log(archEnemies)
 // Remove the first sidekick from the sidekicks array
 archEnemies.pop
 // Print the sidekicks array to console to ensure you added a new sidekick
-console.log(archEnemies)
+console.log(sideKicks)
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
 //   - saveTheDay should be a string a hero would say once they save the day 
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
-var = function {
-dangerLevel: 5
+function assesSituation(dangerLevel, saveTheDay, badExcuse) {
+dangerLevel: > 50
 saveTheDay: "I saved the day!"
 badExcuse: "I am afriad!"
 }
@@ -63,6 +63,20 @@ badExcuse: "I am afriad!"
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
 //   - Anything dangerLevel that is between 10 and 50 should result in printing the saveTheDay string to the console
 //   - If the dangerLevel is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the console.
+
+var assesSituation = 7;
+
+if (dangerLevel > 51) {
+    console.log(badExcuse);
+} else if (dangerLevel <= 50) {
+    console.log(saveTheDay);
+} else if (dangerLevel < 10) {
+    console.log(" Meh. Hard pass. ");
+} else {
+    console.log("");
+};
+
+
 
 //Test Cases
 var announcement = 'Never fear, the Courageous Curly Bracket is here!';
@@ -79,6 +93,16 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - luckyNumbers (array)
 //   - address (object with following key/values: number , street , state, zip)
 
+var scaryMonster  = {
+
+name: "Death",
+smell: "garbage",
+weight: 10,
+citiesDestroyed: ["cleavland", "Frederick", "D.C",
+luckyNumbers: [5, 10, 15],
+address.function(number, street, state, zip){
+console.log(number, street, state, zip);
+}
 
 // Create a new class called SuperHero
 // - Your class should have the following DYNAMIC values
@@ -90,18 +114,40 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - powerLevel = 100
 //   - energyLevel = 50 
 
+
+class SuperHero {
+  constructor(name, superPower, age ) {
+    this.name = name;
+    this.superPower = superPower;
+    this.age = age;
+    this.archNemesis = "The Syntax Error;
+    this.powerLevel = 100;
+    this.enerygLevel = 50;
+  }
+}
+
+
+
 // - Create the following class methods
 //   - sayName, should print the hero's name to the console
 //   - maximizeEnergy, should update the energyLevel to 1000
 //   - gainPower, should take an argument of a number and INCREASE the powerLevel by that number
 
-// - Create 2 instances of your SuperHero class
+var sayName = new SuperHero("Henry")
+console.log(name);
+
+var maximizeEnergy = new SuperHero(1000)
+console.log(1000);
+var gainPower = new SuperHero(
+
+// I do not know how to do this part... It has taken me much longer than 30 minutes... 
+
 
 
 // Reflection
 // What parts were most difficult about this exerise?
-
+Creating the class. I really need a tutoring session on this. 
 // What parts felt most comfortable to you?
-
+defining variables are second nature now. 
 // What skills do you need to continue to practice before starting Mod 1?
-
+classes. if/else statements. I need a lot of practice on this. 
